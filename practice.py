8194200c83a70b9f61ddf6544c5ccb92fdb31d7a
Dqrk <133540030+Dqrkmoon@users.noreply.github.com>
2024-11-01 21:29:22 +0530

    Rock Paper Scissor game

--- practice.py
+++ practice.py
@@ -1,24 +1,47 @@
-# dict = {}
+import random as rd
 
-# name = input("Enter your name: ")
-# language = input("Enter your fav language: ")
-
-# dict.update({name : language})
-
-# print(dict)
-
-def entry():
-    dict = {}
+def game():
     while True:
-        name = input("Enter name: ")
-        language = input("Enter their fav language: ")
-        dict.update({name : language})
+        com_choice = ("rock" , "paper" , "scissor")
+        user_choice = input("Enter rock, paper or scissor: ").lower()
+        computer_choice = rd.choice(com_choice)
 
-        option = input("Do you want to enter more value? (y/n): ").lower()
+        if computer_choice == user_choice:
+            print(f"Your choice: {user_choice} \n Computer choice: {computer_choice}")
+            print("This is bet!!")
 
-        if option == "n":
-            print(dict.items())
+        elif computer_choice == "rock" and user_choice == "paper":
+            print(f"Your choice: {user_choice} \n Computer choice: {computer_choice}")
+            print("You wins!!")
+    
+        elif computer_choice == "rock" and user_choice == "scissor":
+            print(f"Your choice: {user_choice} \n Computer choice: {computer_choice}")
+            print("Computer wins!!")
+    
+        elif computer_choice == "paper" and user_choice == "rock":
+            print(f"Your choice: {user_choice} \n Computer choice: {computer_choice}")
+            print("Computer wins!!")
+
+        elif computer_choice == "paper" and user_choice == "scissor":
+            print(f"Your choice: {user_choice} \n Computer choice: {computer_choice}")
+            print("You wins!!")
+    
+        elif computer_choice == "scissor" and user_choice == "rock":
+            print(f"Your choice: {user_choice} \n Computer choice: {computer_choice}")
+            print("You wins!!")
+
+        elif computer_choice == "scissor" and user_choice == "paper":
+            print(f"Your choice: {user_choice} \n Computer choice: {computer_choice}")
+            print("Computer wins!!")
+        
+        else:
+            print("Enter valid choice")
+
+        again = input("Do you want to try again? (y/n): ").lower()
+
+        if again == "n":
+            print("Thankyou for playing...")
             break
-            
-entry()
 
+
+game()
